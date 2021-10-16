5a601e292cb85954c7d68987dc7d97c1c52e8495
xuan <danielopg11@gmail.com>
2021-10-16 15:44:00 +0800

    Add playfair_encode

--- playfair_encode.cpp
+++ playfair_encode.cpp
@@ -1,8 +1,27 @@
+#include<vector>
 #include<iostream>
 #include<string>
 using namespace std;
+
+vector<char> table[5][5];
+
+void table_for_check(string key,char table[5][5]){
+    
+}
+
+
+
+
 int main(){
     string key, text;
-    cout << "hello world";
+    cout << "key :";
+    cin >> key;
+    cout << "text :";
+    cin >> text;
+
+
+
+
+
     system("pause");
 }
